404bdcc107d2347369f56afbc7daea1bc1a3a868
Anders Kvist <anderskvist@gmail.com>
2022-08-13 16:39:23 +0200

    Creating control node.

--- control.js
+++ control.js
@@ -4,6 +4,13 @@ module.exports = function(RED) {
         var node = this;
         node.on('input', function(msg) {
             node.send(msg);
+            if (msg.payload == "ON") {
+                node.status({fill:"green",shape:"dot",text: config.identifier + " enabled"});
+                node.context().flow.set("easy-disable-" + config.identifier, true);
+            } else {
+                node.status({fill:"red",shape:"dot",text: config.identifier + " disabled"});
+                node.context().flow.set("easy-disable-" + config.identifier, false);
+            }
         });
     }
     RED.nodes.registerType("control",ControlNode);
