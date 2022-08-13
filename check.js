d273753a36b4fed70a5003822f34b2b334fa8b67
Anders Kvist <anderskvist@gmail.com>
2022-08-13 16:47:31 +0200

    Creating check node.

--- check.js
+++ check.js
@@ -3,7 +3,12 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            node.send(msg);
+            if (node.context().flow.get("easy-disable-" + config.identifier) == true) {
+                node.status({fill:"green",shape:"dot",text: config.identifier + " enabled"});
+                node.send(msg);
+            } else {
+                node.status({fill:"red",shape:"dot",text: config.identifier + " disabled"});
+            }
         });
     }
     RED.nodes.registerType("check",CheckNode);
